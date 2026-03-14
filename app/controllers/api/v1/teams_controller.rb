3bf7d4e4b7d83c028edd5bdb8dda388328e9e354
victor1cs <victor1cs@outlook.com>
2026-03-14 16:26:27 -0300

    refactor: logic controller

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,29 +1,29 @@
 module Api
   module V1
     class TeamsController < ApplicationController
+      before_action :set_team, only: [:show, :players]
 
       def index
         teams = Team.page(params[:page]).per(params[:per_page])
-
         render json: teams, status: :ok
       end
 
       def show
-        team = Teams::FindService.call(params[:name])
-        if team.present?
-          render json: team, status: :ok
-        else
-          render json: { error: "Team not found" }, status: :not_found
-        end
+        render json: @team, status: :ok
       end
 
       def players
-        team = Teams::FindService.call(params[:name])
-        if team.present?
-          render json: team.players, status: :ok
-        else
-          render json: { error: "Team not found" }, status: :not_found
-        end
+        render json: @team.players, status: :ok
+      end
+
+      private
+
+      def set_team
+        @team = ::Teams::FindService.call(params[:name])
+
+        return if @team.present?
+
+        render json: { error: "Team not found" }, status: :not_found
       end
     end
   end
