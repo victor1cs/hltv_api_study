cd9c4b69dde415701e47246cf0dc9e841d2ddde5
victor1cs <victor1cs@outlook.com>
2026-03-14 17:23:06 -0300

    chore: add safe navigation to player and team serializers

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -2,14 +2,14 @@ class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :just_name, :nickname, :name, :image, :country, :team
 
   def just_name
-    object.just_name
+    object&.just_name
   end
 
   def country
-    object.country.name
+    object&.country&.name
   end
 
   def team
-    object.team.name
+    object&.team&.name
   end
 end
--- app/serializers/team_player_serializer.rb
+++ app/serializers/team_player_serializer.rb
@@ -2,10 +2,10 @@ class TeamPlayerSerializer < ActiveModel::Serializer
   attributes :id, :just_name, :nickname, :name, :image, :country
 
   def just_name
-    object.just_name
+    object&.just_name
   end
 
   def country
-    object.country.name
+    object&.country&.name
   end
 end
